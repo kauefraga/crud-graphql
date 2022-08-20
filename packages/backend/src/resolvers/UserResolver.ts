e461c30ffcb54b403a6144237414565d35228b8f
kauefraga <kauefragarodrigues456@gmail.com>
2022-08-20 01:22:08 -0300

    :sparkles: feat: add users query and createUser mutation

---
+++ packages/backend/src/resolvers/UserResolver.ts
@@ -0,0 +1,37 @@
+import { randomUUID } from 'crypto';
+import { Arg, Mutation, Query, Resolver } from 'type-graphql';
+import { User } from '../models/User';
+import { PostgresUsersRepository } from '../repositories/implementations/PostgresUsersRepository';
+import { IUsersRepository } from '../repositories/IUsersRepository';
+import { CreateUserUseCase } from '../useCases/CreateUser/CreateUserUseCase';
+
+@Resolver()
+export class UserResolver {
+  constructor(
+    // Remove `usersRepository`
+    private usersRepository: IUsersRepository,
+    private createUserUseCase: CreateUserUseCase
+  ) {
+    this.usersRepository = new PostgresUsersRepository();
+    this.createUserUseCase = new CreateUserUseCase(
+      this.usersRepository
+    )
+  }
+
+  @Query(() => [User])
+  async users() {
+    // Create a ListUsersUseCase instead
+    return this.usersRepository.findMany();
+  }
+
+  @Mutation(() => User)
+  async createUser(@Arg('name') name: string) {
+    const user = await this.createUserUseCase.execute({
+      id: randomUUID(),
+      name,
+    })
+
+    return user;
+  }
+
+}
